d300dc18fdd2d7643c012bfddc7c6013e46c2048
LilacTree <50721867+LilacTree@users.noreply.github.com>
2019-05-17 15:48:31 -0600

    Add files via upload

--- gifts.js
+++ gifts.js
@@ -1,15 +1,15 @@
 module.exports = [
-    /**
-     * You can add more gifts to this list if the gift you want to use is not listed below
-     * 
-     * id: item id, can be found using the in-game command "/8 partnergifter find"
-     * name: name of the item
-     * cd: cooldown of the item (in seconds)
-     * 
-     */
-    {
-        id: 206049,
-        name: "Puppy Figurine",
-        cd: 2,
-    },
+	/**
+	 * You can add more gifts to this list if the gift you want to use is not listed below
+	 * 
+	 * id: item id, can be found using the in-game command "/8 partnergifter find"
+	 * name: name of the item
+	 * cd: cooldown of the item (in seconds)
+	 * 
+	 */
+	{
+		id: 206049,
+		name: "Puppy Figurine",
+		cd: 2,
+	},
 ]
